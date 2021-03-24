29eb13690acf53862539788099fafa56ae53ecf3
analuizacfreitas <ana.freitas@bandtec.com.br>
2021-03-24 20:19:51 -0300

    add files organization

--- js/main.js
+++ js/main.js
@@ -4,23 +4,22 @@ function converter() {
     var valorEmReais = Number(document.getElementById('inputUser').value);
     var moedaEscolhida = document.getElementById('opcaoMoeda').value;
 
-    div_resultado.innerHTML = `<br><p>Valor digitado (BRL): 
-    R$${valorEmReais}</p>`
+    div_resultado.innerHTML = `<br><p>Valor digitado (BRL): R$${valorEmReais}</p>`
 
     if (moedaEscolhida == 'euro') {
         var cotacaoEuro = 6.54;
         div_resultado.innerHTML += `<p>Valor em Euro (EUR): 
-        €${(valorEmReais * cotacaoEuro).toFixed(2)}</p>`
-    } 
-   if (moedaEscolhida == 'dolar') {
+                                    €${(valorEmReais * cotacaoEuro).toFixed(2)}</p>`
+    } else if (moedaEscolhida == 'dolar') {
         var cotacaoDolar = 5.52;
         div_resultado.innerHTML += `<p>Valor em Dólar (USD): 
-        $${(valorEmReais * cotacaoDolar).toFixed(2)}</p>`
-    }
-    if (moedaEscolhida == 'libra') {
+                                    $${(valorEmReais * cotacaoDolar).toFixed(2)}</p>`
+    } else if (moedaEscolhida == 'libra') {
         var cotacaoLibra = 7.59;
-        div_resultado.innerHTML += `<p>Valor em Libra(GBP): 
-        £${(valorEmReais * cotacaoLibra).toFixed(2)}</p>`
+        div_resultado.innerHTML += `<p>Valor em Libra (GBP): 
+                                    £${(valorEmReais * cotacaoLibra).toFixed(2)}</p>`
+    } else {
+        div_resultado.innerHTML += `<p>Opção inválida. Escolha uma moeda para realizar a conversão.</p>`
     }
 }
 
